1d4709ff28bb8e67f6064a74c1c5f76185c4e021
SirArthur0 <arthurlmshell@gmail.com>
2022-09-19 06:26:19 -0300

    enviado para o git

---
+++ src/test/java/br/com/site_de_vendas/site_de_vendas/SiteDeVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.site_de_vendas.site_de_vendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiteDeVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
